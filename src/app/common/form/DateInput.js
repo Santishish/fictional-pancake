25f51b36a51dfb9d274745e767a044316a747dea
Santi <csantiago2706@hotmail.com>
2018-06-24 20:21:49 -0600

    End of Section 16

--- src/app/common/form/DateInput.js
+++ src/app/common/form/DateInput.js
@@ -4,11 +4,14 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
 
-const DateInput = ({input: {value, onChange, ...restInput}, width, placeholder, meta: {touched, error}, ...rest}) => {
+const DateInput = ({input: {value, onChange, onBlur, ...restInput}, width, placeholder, meta: {touched, error}, ...rest}) => {
+    if (value) {
+        value = moment(value, 'X');
+    }
     return (
         <Form.Field error={touched && !!error} width={width}>
             <DatePicker {...rest} placeholderText={placeholder} selected={value ? moment(value) : null}
-                        onChange={onChange} {...restInput}/>
+                        onBlur={() => onBlur()} onChange={onChange} {...restInput}/>
             {touched && error && <Label basic color='red'>{error}</Label>}
         </Form.Field>
     );
--- src/features/user/Settings/SettingsDashboard.js
+++ src/features/user/Settings/SettingsDashboard.js
@@ -10,15 +10,16 @@ import BasicPage from "./BasicPage";
 import AboutPage from "./AboutPage";
 import PhotosPage from "./PhotosPage";
 import AccountPage from "./AccountPage";
+import {updateProfile} from "../userActions";
 
-const SettingsDashboard = ({updatePassword, providerId}) => {
+const SettingsDashboard = ({updatePassword, providerId, user, updateProfile}) => {
     return (
         <Grid>
             <Grid.Column width={12}>
                 <Switch>
                     <Redirect exact from="/settings" to="/settings/basic"/>
-                    <Route path="/settings/basic" component={BasicPage}/>
-                    <Route path="/settings/about" component={AboutPage}/>
+                    <Route path="/settings/basic" render={() => <BasicPage updateProfile={updateProfile} initialValues={user}/>}/>
+                    <Route path="/settings/about" render={() => <AboutPage updateProfile={updateProfile} initialValues={user}/>}/>
                     <Route path="/settings/photos" component={PhotosPage}/>
                     <Route path="/settings/account"
                            render={() => <AccountPage providerId={providerId} updatePassword={updatePassword}/>}/>
@@ -32,11 +33,13 @@ const SettingsDashboard = ({updatePassword, providerId}) => {
 };
 
 const mapStateToProps = (state) => ({
-    providerId: state.firebase.auth.providerData[0].providerId
+    providerId: state.firebase.auth.providerData[0].providerId,
+    user: state.firebase.profile
 });
 
 const mapDispatchToProps = {
-    updatePassword
+    updatePassword,
+    updateProfile
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SettingsDashboard);
